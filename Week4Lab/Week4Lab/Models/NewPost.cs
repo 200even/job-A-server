18a64828b1436dc6a7298334f132524c8039bceb
Jason W <williams.jasonmd@gmail.com>
2015-08-21 14:15:20 -0500

    added images

---
+++ Week4Lab/Week4Lab/Models/NewPost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Week4Lab.Models
+{
+    public class NewPost
+    {
+        public string Id { get; set; }
+        
+    }
+}
